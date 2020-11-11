d919d0f3926a6d48c8927bb561501749d2279655
Sajan Kc <sajan1996kc@gmail.com>
2020-11-11 22:16:37 +0545

    api url renamed to suitable url

--- src/main/java/com/kcsajan/main/controller/StudentController.java
+++ src/main/java/com/kcsajan/main/controller/StudentController.java
@@ -4,35 +4,36 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kcsajan.main.model.Student;
 import com.kcsajan.main.repository.StudentRepository;
 
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
 @RestController
+@RequestMapping("/student")
 public class StudentController {
 
 	@Autowired
 	private StudentRepository studentRepository;
 
-	@GetMapping(value = "/student")
+	@GetMapping
 	public List<Student> getStudents() {
 		return studentRepository.findAll();
 	}
 
-	@GetMapping(value = "/student/{id}")
+	@GetMapping(value = "/{id}")
 	public Optional<Student> getStudent(@PathVariable long id) {
 		return studentRepository.findById(id);
 	}
 
-	@PostMapping(value = "/addstudent")
+	@PostMapping
 	public String saveStudent(@RequestBody Student student) {
 		Student addedStudent = studentRepository.save(student);
 		return "Student " + addedStudent.getFname() + " " + addedStudent.getLname() + " Added To The Database.";
@@ -48,7 +49,7 @@ public class StudentController {
 //		}
 //	}
 
-	@DeleteMapping(value = "/student/{id}")
+	@DeleteMapping(value = "/{id}")
 	public String deleteStudent(@PathVariable long id) {
 		studentRepository.deleteById(id);
 		return "Student With id : " + id + " Deleted From Database.";
